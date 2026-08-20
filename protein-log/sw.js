06b6026ac536bd599665d045ccd4acae485099ff
prinshen <jacob.laust@gmail.com>
2026-08-20 18:54:30 +0200

    Prepare Protein Log v2

--- protein-log/sw.js
+++ protein-log/sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'protein-log-v11';
-const ASSETS = ['./','./index.html','./styles.css?v=10','./app.js?v=11','./manifest.webmanifest','./icon.svg'];
+const CACHE = 'protein-log-v12';
+const ASSETS = ['./','./index.html','./styles.css?v=12','./app.js?v=12','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)));self.skipWaiting()});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim()});
 self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;event.respondWith(fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response}).catch(()=>caches.match(event.request).then(r=>r||caches.match('./index.html'))))});
