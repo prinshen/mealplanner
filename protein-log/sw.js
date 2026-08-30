ebedc3bfa8c3a62bf75c248104531e7cfef8a52e
prinshen <jacob.laust@gmail.com>
2026-08-30 15:49:48 +0200

    Refresh Protein Log offline cache for version 15

--- protein-log/sw.js
+++ protein-log/sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'protein-log-v14';
-const ASSETS = ['./','./index.html','./styles.css?v=14','./app.js?v=14','./manifest.webmanifest','./icon.svg'];
+const CACHE = 'protein-log-v15';
+const ASSETS = ['./','./index.html','./styles.css?v=15','./app.js?v=15','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)));self.skipWaiting()});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim()});
 self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;event.respondWith(fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response}).catch(()=>caches.match(event.request).then(r=>r||caches.match('./index.html'))))});
