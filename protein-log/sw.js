98ab11b7a8cf22b737b747e51d0f8a8c71ab2419
prinshen <jacob.laust@gmail.com>
2026-09-17 22:20:14 +0200

    Bump Protein Log cache to v17.5

--- protein-log/sw.js
+++ protein-log/sw.js
@@ -1,6 +1,6 @@
 const PREFIX = 'protein-log-';
-const CACHE = PREFIX + 'v17.4';
-const ASSETS = ['./', './index.html', './styles.css?v=17.4', './app.js?v=17.4', './manifest.webmanifest', './icon.svg'];
+const CACHE = PREFIX + 'v17.5';
+const ASSETS = ['./', './index.html', './styles.css?v=17.5', './app.js?v=17.5', './manifest.webmanifest', './icon.svg'];
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting()));
 });
